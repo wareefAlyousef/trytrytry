5e42a8ed24df6aa894ea5f9d96c1088b3d4a53c5
wareefAlyousef <144881996+wareefAlyousef@users.noreply.github.com>
2024-02-23 16:56:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
             echo "<br><p>from waref try try try</p>";
             echo "<p>this echo from anathor divise</p>";
             echo "<p>this echo from the first divise</p>";
+            echo "<p>this echo from anathor divise</p>";
         ?>
     </body>
 </html>
