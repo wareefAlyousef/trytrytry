f2e0fedc724374c2facb51d8a944872bd1684fd1
wareefAlyousef <144881996+wareefAlyousef@users.noreply.github.com>
2024-02-23 16:56:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
             echo "<h1>hellow world</h1>";
             echo "<br><p>from waref try try try</p>";
             echo "<p>this echo from anathor divise</p>";
+            echo "<p>this echo from the first divise</p>";
         ?>
     </body>
 </html>
