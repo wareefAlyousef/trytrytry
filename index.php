773f14952ea0b7fffb93bd7298845746f158a6ad
wareefAlyousef <144881996+wareefAlyousef@users.noreply.github.com>
2024-02-23 16:50:55 +0300

    from anathor dvise

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         <?php
             echo "<h1>hellow world</h1>";
             echo "<br><p>from waref try try try</p>";
+            echo "<p>this echo from anathor divise</p>";
         ?>
     </body>
 </html>
