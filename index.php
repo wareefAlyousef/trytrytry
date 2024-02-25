b7e56989178213e870e1f7701d0d715a4e821841
BashairAlsadhan <bashair.sadhan@gmail.com>
2024-02-25 20:40:21 +0300

    add connection to DB

--- index.php
+++ index.php
@@ -43,6 +43,18 @@
             echo "<p>this echo from anathor divise</p>";
             echo "IT Woooooooooooooorked on Rana's device ;) ^^";
             echo "shokraaannn wareeefffffff";
+            
+            $connection = mysqli_connect("localhost","root","root","webdb");
+            $error = mysqli_connect_error();
+            
+            if($error != null){
+                echo '<p> cant connect to DB';
+                
+            }
+            else{
+                 echo '<p> connect to DB';
+            }
+            
         ?>
 
         
